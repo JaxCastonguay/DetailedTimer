09ffd3ab6d7f42e40bb239ae060f3b72ce92ca69
Jacques <jgc009@gmail.com>
2020-05-28 21:08:37 -0500

    Init

---
+++ app/src/main/java/io/github/jaccastonguay/detailedtimer/WorkOut.java
@@ -0,0 +1,27 @@
+package io.github.jaccastonguay.detailedtimer;
+
+public class WorkOut {
+    private String name;
+    private String time;
+
+    public WorkOut(String name, String time) {
+        this.name = name;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
